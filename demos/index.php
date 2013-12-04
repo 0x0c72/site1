910dd845fccd9e105794ba931241a0aa4d55e942
Chris Shenkan <cshenkan@shenkan-associates.com>
2013-12-03 22:05:32 -0500

    updates

---
+++ demos/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+	 
+	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	 <!-- Piwik --> 
+  		<script type="text/javascript">
+  		var pkBaseURL = (("https:" == document.location.protocol) ? "https://chs.no-ip.org/piwik/" : "http://chs.no-ip.org/piwik/");
+  		document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+  		</script><script type="text/javascript">
+  		try {
+  		var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
+  		piwikTracker.trackPageView();
+  		piwikTracker.enableLinkTracking();
+  		} catch( err ) {}
+  		</script><noscript><p><img src="http://chs.no-ip.org/piwik/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
+	<!-- End Piwik Tracking Code -->
+	<head>
+	
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+	<link rel="icon" href="favicon.ico" type="image/x-icon" />		
+	 
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	 
+	<meta name="description" content="" />
+	 
+	<meta name="keywords" content="" />
+	 
+	<meta name="author" content="" />
+	 
+	<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+	 
+	<title>1stWebDesigner PHP Template</title>
+	 
+	</head>
+	 
+	    <body>
+	 
+	        <div id="wrapper">
+	 
+	<?php include('includes/header.php'); ?>
+	 
+	<?php include('includes/nav.php'); ?>
+	 
+	<div id="content">
+	 
+	<h1>Heading1</h1>
+	<h2>Heading2</h2>
+	<h3>Heading3</h3>
+	<h4>Heading4</h4>
+	<h5>Heading5</h5>
+	 
+	<h3>Paragraph Element</h3>
+	 
+	<p>
+	 
+	Quisque pellentesque sodales aliquam. Morbi mollis neque eget arcu egestas non ultrices neque volutpat. Nam at nunc lectus, id vulputate purus. In et turpis ac mauris viverra iaculis. Cras sed elit a purus ultrices iaculis eget sit amet dolor. Praesent ac libero dolor, id viverra libero. Mauris aliquam nibh vitae eros sodales fermentum. Fusce cursus est varius ante vehicula eget ultrices felis eleifend. Nunc pharetra rutrum nibh et lobortis. Morbi vitae venenatis velit.
+	 
+	</p>
+	 
+	<p>
+	 
+	Quisque pellentesque sodales aliquam. Morbi mollis neque eget arcu egestas non ultrices neque volutpat. Nam at nunc lectus, id vulputate purus. In et turpis ac mauris viverra iaculis. Cras sed elit a purus ultrices iaculis eget sit amet dolor. Praesent ac libero dolor, id viverra libero. Mauris aliquam nibh vitae eros sodales fermentum. Fusce cursus est varius ante vehicula eget ultrices felis eleifend. Nunc pharetra rutrum nibh et lobortis. Morbi vitae venenatis velit.
+	 
+	</p>
+	 
+	<h3>Another Heading Starting Point</h3>
+	 
+	<p>
+	 
+	Quisque pellentesque sodales aliquam. Morbi mollis neque eget arcu egestas non ultrices neque volutpat. Nam at nunc lectus, id vulputate purus. In et turpis ac mauris viverra iaculis. Cras sed elit a purus ultrices iaculis eget sit amet dolor. Praesent ac libero dolor, id viverra libero. Mauris aliquam nibh vitae eros sodales fermentum. Fusce cursus est varius ante vehicula eget ultrices felis eleifend. Nunc pharetra rutrum nibh et lobortis. Morbi vitae venenatis velit.
+	 
+	</p>
+	 
+	<p>
+	 
+	Quisque pellentesque sodales aliquam. Morbi mollis neque eget arcu egestas non ultrices neque volutpat. Nam at nunc lectus, id vulputate purus. In et turpis ac mauris viverra iaculis. Cras sed elit a purus ultrices iaculis eget sit amet dolor. Praesent ac libero dolor, id viverra libero. Mauris aliquam nibh vitae eros sodales fermentum. Fusce cursus est varius ante vehicula eget ultrices felis eleifend. Nunc pharetra rutrum nibh et lobortis. Morbi vitae venenatis velit.
+	 
+	</p>
+	 
+	</div> <!-- end #content -->
+	 
+	<?php include('includes/sidebar.php'); ?>
+	 
+	<?php include('includes/footer.php'); ?>
+	 
+	        </div> <!-- End #wrapper -->
+	 
+	    </body>
+	 
+	</html>
